78952486c03623063f7316c941b4be8e1cdeb12e
Suele <suelen.ads.guimraes@gmail.com>
2022-03-31 14:25:40 -0300

    Ajusta os testes para retornar o tratamento de exception.

--- src/test/java/br/com/banco/test/ContaTest.java
+++ src/test/java/br/com/banco/test/ContaTest.java
@@ -4,6 +4,7 @@ import br.com.banco.Agencia;
 import br.com.banco.Banco;
 import br.com.banco.Conta;
 import br.com.banco.ContaCorrente;
+import br.com.banco.exeptions.ValidaContaException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,24 +17,29 @@ public class ContaTest {
 				new Agencia(123,
 						new Banco("Banco do Brasil", "00000000000")));
 
-		Assertions.assertEquals(new BigDecimal(1100), conta.depositar(new BigDecimal(100)));
+		conta.depositar(new BigDecimal(100));
+		Assertions.assertEquals(new BigDecimal(1100), conta.getSaldo());
 	}
 
 	@Test
-	void deposito_na_propria_conta_valor_zerado() {
+	void depositar_valor_zerado_na_conta() {
 		Conta conta = new ContaCorrente(111, new BigDecimal(1000),
 				new Agencia(123,
 						new Banco("Banco do Brasil", "00000000000")));
 
-		Assertions.assertEquals(new BigDecimal(0), conta.depositar(new BigDecimal(0)));
+		Assertions.assertThrows(ValidaContaException.class,
+				() -> conta.depositar(new BigDecimal(0)),
+				"Valor do deposito não pode ser zerado.");
 	}
 
 	@Test
-	void deposito_na_propria_conta_valor_negativo() {
+	void depositar_valor_negativo_na_conta() {
 		Conta conta = new ContaCorrente(111, new BigDecimal(1000),
 				new Agencia(123,
 						new Banco("Banco do Brasil", "00000000000")));
 
-		Assertions.assertEquals(new BigDecimal(0), conta.depositar(new BigDecimal(-1000)));
+		Assertions.assertThrows(ValidaContaException.class,
+				() -> conta.depositar(new BigDecimal(-100)),
+				"Valor do deposito não pode ser negativo.");
 	}
 }
