568d58f5bc40c220e80c00db2edc5e09a8f8b349
aparecida-gui <suelen.ads.guimaraes@gmail.com>
2022-06-08 12:29:51 -0300

    Adiciona o diretorio banco-digital dentro da pasta projeto-banco-digital e remove o diretorio api-banco-digital.

--- banco-digital/src/main/java/br/com/banco/ContaPoupanca.java
+++
@@ -1,9 +0,0 @@
-package br.com.banco;
-
-import java.math.BigDecimal;
-
-public class ContaPoupanca extends Conta {
-	public ContaPoupanca(int numeroConta, BigDecimal saldo, Agencia agencia) {
-		super(numeroConta, saldo, agencia);
-	}
-}
--- banco-digital/src/main/java/br/com/banco/exeptions/ValidaContaException.java
+++
@@ -1,7 +0,0 @@
-package br.com.banco.exeptions;
-
-public class ValidaContaException extends RuntimeException {
-	public ValidaContaException(String message) {
-		super(message);
-	}
-}
